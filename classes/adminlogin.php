b9b79db077b64d3107fe4e35d0f97d95cab813f9
root <root@erik.tuananh1421999@gmail.com>
2020-04-15 07:23:39 -0400

    website mvc

---
+++ classes/adminlogin.php
@@ -0,0 +1,53 @@
+<?php
+	include '../lib/session.php';
+	Session::checkLogin();
+	include '../lib/database.php';
+	include '../helper/format.php';
+?>
+
+
+<?php
+	/**
+	 * 
+	 */
+	class adminlogin 
+	{
+		private $db;
+		private $fm;
+		public function __construct()
+		{
+			$this->db = new Database();
+			$this->fm = new Format();
+		}
+
+		public function login_admin($adminUser, $adminPass){
+			$adminUser = $this->fm->validation($adminUser);
+			$adminPass = $this->fm->validation($adminPass);
+
+			$adminUser = mysqli_real_escape_string($this->db->link, $adminUser); 
+			$adminPass = mysqli_real_escape_string($this->db->link, $adminPass);
+
+			if(empty($adminUser) || empty($adminPass)){
+				$alert = "User and pass must be not empty";
+				return $alert;
+			}else{
+				$query = "SELECT * FROM tbl_admin WHERE adminUser = '$adminUser' AND adminPass = '$adminPass' LIMIT 1";
+				$result = $this->db->select($query);
+
+				if($result != false){
+
+					$value = $result->fetch_assoc();
+					Session::set('adminlogin', true);
+					Session::set('adminId', $value['adminId']);
+					Session::set('adminUser', $value['adminUser']);
+					Session::set('adminName', $value['adminName']);
+
+					header('Location:index.php');
+				}else{
+					$alert = "User or password is ivalid";
+					return $alert;
+				}
+			}
+		}
+	}
+?>
