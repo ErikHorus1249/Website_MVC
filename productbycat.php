ca07801f5850a7104144ec51eccf919764466b20
Erik_Horus <tuananh1421999@gmail.com>
2020-04-27 15:44:02 +0700

    fix

--- productbycat.php
+++ productbycat.php
@@ -31,15 +31,14 @@
 					?>
 				<div class="grid_1_of_4 images_1_of_4">
 					 <a href="preview-3.php"><img src="admin/upload/<?php echo $result['image']; ?>" alt="" height=150 /></a>
-<<<<<<< HEAD
 					 <h2><?php echo $result['productName'] ?></h2>
 					 <p><?php echo $fm->textShorten($result['product_desc'],50) ?></p>
 					 <p><span class="price"><?php echo $result['price']." $" ?></span></p>
-=======
-					 <h2><?php echo $result['productName']?></h2>
+
+					 <!-- <h2><?php echo $result['productName']?></h2>
 					 <p><?php echo $fm->textShorten($result['product_desc'])?></p>
-					 <p><span class="price"><?php echo $result['price']." $"?></span></p>
->>>>>>> 5dde9c915d0e774db9a9dd87e586021bd9e3421a
+					 <p><span class="price"><?php echo $result['price']." $"?></span></p> -->
+
 				     <div class="button"><span><a href="details.php?productid=<?php echo $result['productId'] ?>" class="details">Details</a></span></div>
 				</div>
 				<?php
