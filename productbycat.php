d94956edf5beafb4f01b03408f50c1b8d23005b6
Erik_Horus <tuananh1421999@gmail.com>
2020-04-27 15:24:06 +0700

    Xong phan hien thi san pham

--- productbycat.php
+++ productbycat.php
@@ -6,14 +6,22 @@
 	if(isset($_GET['catid'])){
 		$catId = $_GET['catid'];
 		$productByCat = $product->show_product_by_cat($catId);
+		$productNameByCat = $product->show_product_by_cat($catId);
 	}
 ?>
  <div class="main">
     <div class="content">
     	<div class="content_top">
+				<?php
+						if ($productNameByCat) {
+							$result_name=$productNameByCat->fetch_assoc();
+				?>
     		<div class="heading">
-    		<h3>Latest from <?php  ?></h3>
+    		<h3><?php echo $result_name['catName']?> mới nhất</h3>
     		</div>
+				<?php
+					}
+				?>
     		<div class="clear"></div>
     	</div>
 	      <div class="section group">
@@ -23,10 +31,10 @@
 					?>
 				<div class="grid_1_of_4 images_1_of_4">
 					 <a href="preview-3.php"><img src="admin/upload/<?php echo $result['image']; ?>" alt="" height=150 /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-					 <p><span class="price">$505.22</span></p>
-				     <div class="button"><span><a href="preview.php" class="details">Details</a></span></div>
+					 <h2><?php echo $result['productName'] ?></h2>
+					 <p><?php echo $fm->textShorten($result['product_desc'],50) ?></p>
+					 <p><span class="price"><?php echo $result['price']." $" ?></span></p>
+				     <div class="button"><span><a href="details.php?productid=<?php echo $result['productId'] ?>" class="details">Details</a></span></div>
 				</div>
 				<?php
 					}
